38481e978fdd8caaa6290dbb71e916e5b1baa56b
Jean Carlos Rodriguez Tavarez <jeanrodriguez@presidencia.gob.do>
2023-04-03 16:06:12 -0400

    creating views and js file with jquery to retreive data from opendb api to complete missing game categories

---
+++ js/Books.js
@@ -0,0 +1,50 @@
+import { shuffle } from "./script.js";
+
+let answers = [];
+
+function calltriviapi(){
+
+    $.ajax({
+        type: "GET",
+        url: "https://opentdb.com/api.php?amount=1&category=10&encode=base64",
+        data: "data",
+        dataType: "json",
+    }).done(function(data){
+        $.each(data.results, function(index) {
+    
+            // changing text of button id question to match what the api gives
+            $("#question").text(atob(data.results[index].question));
+    
+            //add correct_answer to the array
+            answers = [{value: 'correct_answer', text: data.results[index].correct_answer}]
+            
+            //add every incorrect answer to the array
+            $.each(data.results[index].incorrect_answers, function(index, value){
+                answers.push({value: 'Incorrect_answer', text: value})
+                console.log(value);
+            })
+            
+            answers = shuffle(answers);
+    
+            $.each(answers, function(index){
+                var append_me = '<button id="answer" class="btn-answers '+answers[index].value+'"> '+ atob(answers[index].text) +' </button>'
+                $("#Answers").append(append_me);
+            });
+        });
+    
+    }).fail(function(){
+        alert("Something went wrong, please try again");
+    });
+    
+}
+
+calltriviapi();
+
+
+$("#div-btn-question").on( "click", 'button.btn-next' ,function() {
+    $('#question').html('');
+    $('#Answers').empty();
+    calltriviapi();
+});
+
+
